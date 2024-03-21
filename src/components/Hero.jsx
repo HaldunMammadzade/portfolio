6ade756203b88cf9326d6c9205385f0d5f4304bf
Haldun Mammadzada <haldun.mammadzada@gmail.com>
2024-03-21 16:48:40 +0400

    lalala

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -42,7 +42,7 @@ const Hero = () => {
                 className="sm:text-battleGray sm:text-[90px] 
                 text-eerieBlack text-[50px] font-mova
                 font-extrabold uppercase">
-                Shaquille
+                Haldun 
               </span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-eerieBlack`}>
@@ -83,9 +83,7 @@ const Hero = () => {
         {/* Your image comes here. Feel free to remove image if you don't plan to have one.*/}
         <div>
           <img
-            className="absolute bottom-0 ml-[50vw] 
-            lg:ml-[75vw] md:ml-[60vw] xmd:ml-[60vw] 2xl:ml-[83vw]
-            sm:h-[90vh] md:h-[70vh] xl:h-[80vh]"
+            className="absolute main-img "
             src={shaq}
             alt="shaquille"
           />
